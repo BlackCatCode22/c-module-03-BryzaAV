a685739883076503df710d7f6e84fdf73b660038
BryzaAV <156037120+BryzaAV@users.noreply.github.com>
2025-09-08 12:16:14 -0700

    Add files via upload

---
+++ numberGrid.cpp
@@ -0,0 +1,18 @@
+//numberGrid.cpp
+//BAV 9/8/2025
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //fill the array
+    int numberGrid[3][2] = {
+        {1, 2},
+        {3, 4},
+        {5, 6}
+    };
+
+    cout << numberGrid[0][0];
+    return 0;
+}
